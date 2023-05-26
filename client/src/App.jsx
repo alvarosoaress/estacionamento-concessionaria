315d51cea8f6774afa89ac27fa260a8b310f5263
alvarosoares01@gmail.com <alvarosoares01@gmail.com>
2023-05-26 12:51:42 -0300

    commit

---
+++ client/src/App.jsx
@@ -0,0 +1,510 @@
+import { useEffect, useState } from 'react';
+import React from 'react';
+import axios from 'axios';
+
+import { IoMdArrowDropdown } from 'react-icons/io';
+
+import Select from 'react-select';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
+
+import './App.css';
+
+function App() {
+  const API = import.meta.env.VITE_API;
+
+  // states para controlar qual coluna da tabela detalhes está sendo ordenada
+  const [filtroArea, setFiltroArea] = useState(false);
+  const [filtroOcupacao, setFiltroOcupacao] = useState(false);
+
+  const [automoveis, setAutomoveis] = useState([]);
+
+  // info de todos automoveis dentro de X area
+  const [automoveisArea, setAutomoveisArea] = useState([]);
+
+  const [concessionarias, setConcessionarias] = useState([]);
+
+  // infos das concessionarias tratadas para utilizar no Select de venda
+  const [concOpcoes, setConcOpcoes] = useState([]);
+
+  const [clientes, setClientes] = useState([]);
+
+  // infos dos clientes tratados para utilizar no Select de venda
+  const [clientesOpcoes, setClientesOpcoes] = useState([]);
+
+  const [areas, setAreas] = useState({});
+  // newAreas para melhor manipulação quando ordenada a tabela
+  const [newAreas, setNewAreas] = useState({});
+
+  // id de qual area foi clicada
+  const [idArea, setIdArea] = useState(0);
+
+  const [vendas, setVendas] = useState([]);
+
+  // exibir o modal
+  const [modal, setModal] = useState(false);
+
+  // mudar o que é exibido no modal
+  const [venda, setVenda] = useState(false);
+
+  // id do automovel selecionado para venda
+  const [idAutoVenda, setIdAutoVenda] = useState(null);
+
+  function formatarMoeda(value) {
+    // Intl para formatar para BRL o preço
+    let BRreal = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+      minimumFractionDigits: 2,
+    });
+
+    // formatando o input de preço dentro da variável value
+    // formatar valor em moeda BRL
+    return BRreal.format(value);
+  }
+
+  useEffect(() => {
+    const getAutomoveis = async () => {
+      try {
+        const res = await axios.get(`${API}/automoveis`);
+
+        // object para facilitar a manipulação
+        const object = {};
+        res.data.forEach((auto) => {
+          object[auto.id] = auto;
+        });
+        setAutomoveis(object);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const getAreas = async () => {
+      try {
+        const res = await axios.get(`${API}/areas`);
+
+        // object para facilitar a manipulação
+        const object = {};
+        res.data.forEach((area) => {
+          object[area.area] = area;
+        });
+        setAreas(object);
+        setNewAreas(object);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const getClientes = async () => {
+      try {
+        const res = await axios.get(`${API}/clientes`);
+
+        setClientes(res.data);
+
+        // criando opcoes para o select usado na venda
+        let opcoes = [];
+
+        res.data.map((cliente) => {
+          opcoes.push({
+            value: cliente.id,
+            label: cliente.nome,
+          });
+        });
+
+        setClientesOpcoes(opcoes);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const getConcessionarias = async () => {
+      try {
+        const res = await axios.get(`${API}/concessionarias`);
+
+        setConcessionarias(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const getVendas = async () => {
+      try {
+        const res = await axios.get(`${API}/venda`);
+
+        setVendas(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getAutomoveis();
+    getClientes();
+    getConcessionarias();
+    getAreas();
+    getVendas();
+  }, []);
+
+  useEffect(() => {
+    const getAreaById = async () => {
+      try {
+        const res = await axios.get(`${API}/areas/${idArea}`);
+
+        setAutomoveisArea(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getAreaById();
+  }, [idArea]);
+
+  useEffect(() => {
+    document.body.style.height = modal ? '100%' : 'auto';
+    document.body.style.overflow = modal ? 'hidden' : 'visible';
+  }, [modal]);
+
+  useEffect(() => {
+    const getConcessionariasByAuto = async () => {
+      try {
+        const res = await axios.get(`${API}/concessionarias/${idAutoVenda}`);
+        // criando opcoes para o select usado na venda
+        let opcoes = [];
+
+        res.data.map((conc) => {
+          opcoes.push({
+            value: conc.id,
+            label: conc.nome,
+          });
+        });
+
+        setConcOpcoes(opcoes);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getConcessionariasByAuto();
+  }, [idAutoVenda]);
+
+  async function realizarVenda(event) {
+    const body = {
+      clienteId: event.target.elements['clienteId'].value,
+      concId: event.target.elements['concId'].value,
+      autoId: idAutoVenda,
+    };
+
+    try {
+      await axios.post(`${API}/venda`, body);
+      await axios.put(`${API}/automovelqtd`, {
+        autoId: idAutoVenda,
+        areaId: idArea,
+      });
+      setModal(false);
+      setVenda(false);
+      setIdArea(0);
+      toast.success('Automóvel vendido com sucesso!');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // customStyles para os Selects
+  const customStyles = {
+    // option = dropDown menu com as opções
+    option: (defaultStyles, state) => ({
+      ...defaultStyles,
+      color: state.isSelected ? '#ffffff' : '#707070',
+      backgroundColor: state.isSelected ? '#7814FF' : '#ffffff',
+      ':hover': {
+        backgroundColor: '#707070',
+        color: 'white',
+      },
+    }),
+
+    // control = input estático (sem foco)
+    control: (defaultStyles) => ({
+      ...defaultStyles,
+      backgroundColor: '#ffffff',
+      border: '1px solid gray',
+      boxShadow: 'none',
+      height: '40px',
+      width: '100%',
+      color: '#000000',
+      ':hover': {
+        border: '1px solid black',
+      },
+    }),
+
+    // singleValue = valor selecionado no input
+    singleValue: (defaultStyles) => ({
+      ...defaultStyles,
+    }),
+  };
+
+  const OpcaoArea = ({ display }) => {
+    if (automoveisArea.length <= 0) {
+      return (
+        <>
+          <h1>Área {idArea}</h1>
+          <h3 className="areaVazia">Área está vazia</h3>
+        </>
+      );
+    }
+    return (
+      <div style={{ display: display }}>
+        <h1>Área {idArea}</h1>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Modelo</th>
+                <th>Preço</th>
+                <th>Qtd.</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.values(automoveisArea).map((auto, index) =>
+                // mostrando paenas automoveis com qtd maior do que 0
+                auto.quantidade > 0 ? (
+                  <tr key={index}>
+                    <td>{auto.modelo}</td>
+                    <td>{formatarMoeda(auto.preco)}</td>
+                    <td>{auto.quantidade}</td>
+                    <td>
+                      <button
+                        onClick={() => {
+                          setVenda(true), setIdAutoVenda(auto.id);
+                        }}
+                      >
+                        Vender
+                      </button>
+                    </td>
+                  </tr>
+                ) : (
+                  ''
+                ),
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  };
+
+  const OpcaoVenda = ({ display }) => {
+    return (
+      <div style={{ display: display }}>
+        <h1>Venda</h1>
+        <form
+          method="POST"
+          id="form"
+          className="labelContaier"
+          onSubmit={(event) => {
+            event.preventDefault(), realizarVenda(event);
+          }}
+        >
+          <label>Cliente:</label>
+          <Select
+            name="clienteId"
+            className="selectComponent"
+            options={clientesOpcoes}
+            styles={customStyles}
+            placeholder="Selecione..."
+            noOptionsMessage={() => 'Nenhum resultado'}
+            required
+          />
+
+          <label>Concessionaria:</label>
+          <Select
+            name="concId"
+            className="selectComponent"
+            options={concOpcoes}
+            styles={customStyles}
+            placeholder="Selecione..."
+            noOptionsMessage={() => 'Nenhum resultado'}
+            required
+          />
+        </form>
+
+        <div className="botoesVenda">
+          <button form="form" type="submit">
+            Confirmar
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      {/* Popup superior direito  */}
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover
+        theme="light"
+      />
+      <div
+        className="modalContainer"
+        style={{
+          display: modal ? 'flex' : 'none',
+          top: window.pageYOffset,
+        }}
+        onClick={() => {
+          setModal(false), setVenda(false);
+        }}
+      >
+        {/* stopPropagation para evitar que ao cliclar no modal interno
+        todo o modal se feche (parando a propagação do evento clique para o elemento pai) */}
+        <div className="modal" onClick={(e) => e.stopPropagation()}>
+          <OpcaoArea display={venda ? 'none' : 'block'} />
+          <OpcaoVenda display={venda ? 'block' : 'none'} />
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="gridContainer">
+          {
+            // Gerando as 10 áreas
+            [...Array(10)].map((_, index) => (
+              <div
+                className="area"
+                key={index + 1}
+                style={{
+                  gridArea: `area${index + 1}`,
+                  backgroundColor:
+                    areas[index + 1]?.ocupacao > 0 ? '#0000FF' : '#FFF',
+                  color: areas[index + 1]?.ocupacao > 0 ? '#FFF' : '#0000FF',
+                }}
+                onClick={() => {
+                  setIdArea(index + 1), setModal(true);
+                }}
+              >
+                {index + 1}
+              </div>
+            ))
+          }
+        </div>
+        <div className="tabelasContainer">
+          <div className="tabelaBox">
+            <h1>Detalhes</h1>
+            <div className="tabelaDetalhes">
+              <table>
+                <thead>
+                  <tr>
+                    <th
+                      onClick={() => {
+                        filtroOcupacao ? setFiltroOcupacao(false) : '';
+                        if (filtroArea) {
+                          setNewAreas({ ...areas });
+                          setFiltroArea(false);
+                        } else {
+                          setNewAreas({
+                            ...Object.values(areas).sort(
+                              (a, b) => b.area - a.area,
+                            ),
+                          });
+                          setFiltroArea(true);
+                        }
+                      }}
+                    >
+                      Área{' '}
+                      <IoMdArrowDropdown
+                        size={25}
+                        style={{
+                          transition: 'all 200ms ease-in-out',
+                          rotate: filtroArea ? '0deg' : '180deg',
+                        }}
+                      />
+                    </th>
+                    <th
+                      onClick={() => {
+                        filtroArea ? setFiltroArea(false) : '';
+                        if (filtroOcupacao) {
+                          setNewAreas({ ...areas });
+                          setFiltroOcupacao(false);
+                        } else {
+                          setNewAreas({
+                            ...Object.values(areas).sort(
+                              (a, b) => b.ocupacao - a.ocupacao,
+                            ),
+                          });
+                          setFiltroOcupacao(true);
+                        }
+                      }}
+                    >
+                      Ocupação{' '}
+                      <IoMdArrowDropdown
+                        size={25}
+                        style={{
+                          transition: 'all 200ms ease-in-out',
+                          rotate: filtroOcupacao ? '0deg' : '180deg',
+                        }}
+                      />
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* Object.values para o Objeto de Objetos virar um "array"
+                 assim sendo possível realizar o map */}
+                  {Object.values(newAreas).map((area, index) => (
+                    <tr key={index}>
+                      <td>{area.area}</td>
+                      <td>{area.ocupacao}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div className="tabelaBox" style={{ marginBlock: '5%' }}>
+            <h1>Vendas</h1>
+            <div className="tabelaDetalhes">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Automóvel</th>
+                    <th>Cliente</th>
+                    <th>Concessionária</th>
+                    <th>Data</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {console.log(clientes)}
+                  {vendas.map((venda, index) => (
+                    <tr key={index}>
+                      <td>{automoveis[venda.auto].modelo}</td>
+                      <td>
+                        {
+                          clientes.find(
+                            (cliente) => cliente.id == venda.cliente,
+                          ).nome
+                        }
+                      </td>
+                      <td>
+                        {concessionarias
+                          .find((conc) => conc.id == venda.conc)
+                          .nome.replace('concessionária', '')}
+                      </td>
+                      <td>{new Date(venda.data).toLocaleString('pt-br')}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,183 @@
+import express from 'express'
+import mysql from 'mysql2' // usando lib mysql2 para maior compatibilidade
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+dotenv.config(); // configurando para uso do arquivo .ENV
+
+const db = mysql.createConnection({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE
+})
+
+// COLOCAR PORT EM ARQUIVO ENV
+// COLOCAR HTTP LINK ENV
+  
+
+// Selecionar informações de todos automóveis
+app.get('/automoveis', (req, res)=>{
+    const query = `
+    SELECT auto_id as id, auto_modelo as modelo, auto_preco as valor
+    FROM automovel;
+    `;
+    db.query(query, (err,data)=>{
+        if(err) return res.json(err);
+        return res.status(200).json(data);
+    })
+})
+
+// Selecionar informações de todos clientes
+app.get('/clientes', (req, res)=>{
+    const query = `
+    SELECT cliente_id as id, cliente_nome as nome
+    FROM cliente;
+    `;
+    db.query(query, (err,data)=>{
+        if(err) return res.json(err);
+        return res.status(200).json(data);
+    })
+})
+
+// Selecionar informações de todos concessionarias
+app.get('/concessionarias', (req, res)=>{
+    const query = `
+    SELECT conc_id as id, conc_nome as nome
+    FROM concessionaria;
+    `;
+    db.query(query, (err,data)=>{
+        if(err) return res.json(err);
+        return res.status(200).json(data);
+    })
+})
+
+// Selecionar informações de todos concessionarias 
+// que tenham X carro
+app.get('/concessionarias/:id', (req, res)=>{
+    const autoId = req.params.id;
+    const query = `
+    SELECT a.alocacao_concessionaria as id, c.conc_nome as nome
+    FROM alocacao as a
+    JOIN concessionaria as c ON a.alocacao_concessionaria = c.conc_id
+    WHERE a.alocacao_automovel = ?;
+    `;
+
+    db.query(query, autoId, (err,data)=>{
+        if(err) return res.json(err);
+        return res.status(200).json(data);
+    })
+})
+
+app.get('/areas', (req, res)=>{
+    // CAST utilizado pois SUM retorna NEWDECIMAL, tipo qual o JS converte para STRING
+    // com o CAST para REAL o JS consegue utilizar o resultado de SUM como INTEGER
+    // OBS -> CASO O SUM TENHA PARTES DECIMAIS, ELAS SERÃO ARREDONDADAS POR CAUSA DO CAST !
+    const query = `
+    SELECT alocacao_area as area,CAST(sum(alocacao_quantidade) AS real) as ocupacao
+    FROM alocacao 
+    GROUP BY alocacao_area 
+    ORDER BY area ASC`;
+    db.query(query, (err,data)=>{
+        if(err) return res.json(err);
+        return res.status(200).json(data);
+    })
+})
+
+// Selecionar informações de todos automovéis presente em X área
+app.get('/areas/:id', (req, res)=>{
+    const areaId = req.params.id;
+    const query = `
+    SELECT auto_id as id, auto_modelo as modelo, auto_preco as preco, e.alocacao_quantidade as quantidade
+     FROM alocacao as e
+     JOIN automovel as a ON e.alocacao_automovel = a.auto_id
+     WHERE e.alocacao_area = ?
+     ORDER BY auto_preco;
+    `;
+    db.query(query, areaId ,(err,data)=>{
+        if(err) return res.json(err);
+        return res.status(200).json(data);
+    })
+})
+
+// Selecionar informações de venda de automovel
+app.get('/venda', (req, res) =>{
+    const query = `
+    SELECT venda_id as id, venda_automovel as auto, venda_cliente as cliente, venda_conc as conc, venda_data as data
+    FROM venda;`;
+
+    db.query(query ,(err,data)=>{
+        if(err) return res.json(err);
+        return res.status(200).json(data);
+    })
+})
+
+// Adicionar informações de venda de automovel
+app.post('/venda', (req, res) =>{
+    const query = `
+    INSERT INTO venda (venda_cliente, venda_conc, venda_automovel) VALUES (?)
+    `;
+
+    const values = [
+        req.body.clienteId,
+        req.body.concId,
+        req.body.autoId,
+    ];
+
+    db.query(query, [values] ,(err,data)=>{
+        if(err) return res.json(err);
+        return res.status(201).json("Venda registrada!");
+    })
+})
+
+// Selecionar quantidade de X automovel presente em X área
+app.get('/automovelqtd', (req, res) =>{
+    const query = `
+    SELECT alocacao_quantidade 
+    FROM estacionamento.alocacao 
+    WHERE alocacao_automovel = ? AND alocacao_area = ?;`
+
+    const values = [
+        req.body.autoId,
+        req.body.areaId,
+    ];
+
+    db.query(query, values ,(err,data)=>{
+        if(err) return res.json(err);
+        return res.status(200).json(data);
+    })    
+})
+
+// Atualizar a quantidade do automovel vendido
+app.put('/automovelqtd', (req, res) =>{
+    const query = `
+    UPDATE estacionamento.alocacao
+    SET alocacao_quantidade = alocacao_quantidade-1
+    WHERE alocacao_automovel = ? AND alocacao_area = ?;
+    `;
+
+    const values = [
+        req.body.autoId,
+        req.body.areaId,
+    ];
+
+    db.query(query, values ,(err,data)=>{
+        if(err) return res.json(err);
+        return res.status(201).json("Quantidade de automoveis modificada!");
+    })    
+})
+
+// Rotas 404
+app.use("*", (req, res) => {
+    console.log(`\u001b[31m[ERR] Rota não existente: ${req.baseUrl}`);
+  });
+
+
+
+app.listen(8800, ()=>{
+    console.log('Conectado com sucesso!')
+})
